0880405df3461f9293e475ab01d840dad042eb49
bvdv <bvdv2019@gmail.com>
2018-04-23 21:36:01 +0300

    Add logo

--- header.php
+++ header.php
@@ -25,27 +25,25 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'travelasia' ); ?></a>
 
 	<header class="default-header">
-				<div class="container">
-					<div class="header-wrap">
-						<div class="header-top d-flex justify-content-between align-items-center">
-							<div class="logo">
-								<a href="#home"><img src="img/logo.png" alt=""></a>
-							</div>
-							<div class="main-menubar d-flex align-items-center">
-								<nav class="hide">
-									<a href="#home">Home</a>
-									<a href="#booking">Booking</a>
-									<a href="#package">Packages</a>
-									<a href="#contact">Contact</a>
-								</nav>
-								<div class="menu-bar"><span class="lnr lnr-menu"></span></div>
-							</div>
-						</div>
+		<div class="container">
+			<div class="header-wrap">
+				<div class="header-top d-flex justify-content-between align-items-center">
+					<div class="logo">
+						<a href="#home"><?php the_custom_logo(); ?></a>
 					</div>
+					<div class="main-menubar d-flex align-items-center">
+						<nav class="hide">
+							<a href="#home">Home</a>
+							<a href="#booking">Booking</a>
+							<a href="#package">Packages</a>
+							<a href="#contact">Contact</a>
+						</nav>
+					    <div class="menu-bar"><span class="lnr lnr-menu"></span></div>
+			        </div>
 				</div>
+			</div>
+		</div>
 	</header>
 
 
-
-
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">    	
