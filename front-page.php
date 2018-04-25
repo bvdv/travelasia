c651b16ec9b464854ae17036e073dde03ffc1662
bvdv <bvdv2019@gmail.com>
2018-04-25 11:30:55 +0300

    move LP from index to front

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Travelasia
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		get_template_part( 'page', 'banner' );
+		get_template_part( 'page', 'booking' );
+		//get_template_part( 'page', 'feature' );
+		?>
+		<section class="feature-area section-gap">
+		    <div class="container">
+		        <div class="row">
+		            <?php category_feature_posts(); ?>
+		        </div>
+		    </div>
+		</section>
+		<?php  
+        get_template_part( 'page', 'packages' );
+        get_template_part( 'page', 'blog' );
+        get_template_part( 'page', 'make-pack' );
+        get_template_part( 'page', 'contact' );
+
+		
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
--- index.php
+++ index.php
@@ -19,24 +19,36 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php
-		get_template_part( 'page', 'banner' );
-		get_template_part( 'page', 'booking' );
-		//get_template_part( 'page', 'feature' );
-		?>
-		<section class="feature-area section-gap">
-		    <div class="container">
-		        <div class="row">
-		            <?php category_feature_posts(); ?>
-		        </div>
-		    </div>
-		</section>
-		<?php  
-        get_template_part( 'page', 'packages' );
-        get_template_part( 'page', 'blog' );
-        get_template_part( 'page', 'make-pack' );
-        get_template_part( 'page', 'contact' );
+		if ( have_posts() ) :
 
-		
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
 		?>
 
 		</main><!-- #main -->
